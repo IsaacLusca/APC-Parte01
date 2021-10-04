16b0565028acd37a0ba0c316f3df8d552df60617
IsaacLusca <90662483+IsaacLusca@users.noreply.github.com>
2021-10-04 14:50:06 -0300

    Update annualco2.py

--- annualco2.py
+++ annualco2.py
@@ -1,27 +1,66 @@
+
 import plotly.graph_objects as go
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv('annual-co2-emissions-per-country.csv', sep=',', usecols=['Code', 'Year', 'Annual CO2 emissions'])
+df = pd.read_csv('https://raw.githubusercontent.com/EdPPF/APC-Parte01/main/annual-co2-emissions-per-country(4).csv')
 
+#Transformando a lista em array
 df_array = df.values
 
-#criando listas vazias
-pais = []
+print(df_array)
+#criando listas vazias para manipular depois
 ano = []
-CO2_emitido = []
+asia = []
+brasil = []
+china = []
+europa = []
+alemanha = []
+americsul = []
+usa = []
+world = []
+
 
 #Extrutura de repetição:
 for i in df_array:
-  pais.append(i[0])
-  ano.append(i[1])
-  CO2_emitido.append(i[2])
-  
-  criacao_grafico = go.Scatter(
-    x = ano,
-    y = CO2_emitido
-)
+  ano.append(i[0])
+  asia.append(i[1])
+  brasil.append(i[2])
+  china.append(i[3])
+  europa.append(i[4])
+  alemanha.append(i[5])
+  americsul.append(i[6])
+  usa.append(i[7])
+  world.append(i[8])
+#Criação e edição do gráfico / Eixo x
+#Definindo qual valor será representado no eixo Y.
+#mode define o tipo de linha que o gráfico terá. 
+#text define o padrão que as linhas armazenará, e nesse caso será o nome dos países.
+linha1 = go.Scatter(x=ano, y= asia, mode = 'lines', name = 'Asia')
+linha2 = go.Scatter(x = ano, y = brasil, mode = 'lines', name = 'Brasil')
+linha3 = go.Scatter(x = ano, y = china, mode = 'lines', name = 'China')
+linha4 = go.Scatter(x = ano, y = europa, mode = 'lines', name = 'Europe')
+linha5 = go.Scatter(x=ano, y= alemanha, mode = 'lines', name = 'Germany')
+linha6 = go.Scatter(x = ano, y = americsul, mode = 'lines', name = 'South America')
+linha7 = go.Scatter(x = ano, y = usa, mode = 'lines', name = 'United States')
+linha8 = go.Scatter(x = ano, y = world, mode = 'lines', name = 'World')
 
-grafico = go.Figure(criacao_grafico)
-  grafico.show()
-  
+imagem = [linha1, linha2, linha3, linha4, linha5, linha6, linha7, linha8]
+#Comando que permite a manipulação das características do gráfico
+ 
+#Dict serve para passar mais de uma informação para um argumento
+graf_linha = go.Figure(imagem)
+graf_linha.update_layout(
+    title_text='Emissão de CO2: Alemanha, America do sul, Asia, Brasil, China, Europa, EUA',
+    template = 'plotly_dark',
+    xaxis = dict(
+        title = "Anos"),
+    yaxis = dict(
+        title = 'Concentração de CO2'
+    )
+)  
+#Title representa o "Nome principal" Do gráfico
+#Template altera o tema de fundo do gráfico
+#Xaxis e Yaxis acrescentam, respectivamente, o nome do eixo x e o y do gráfico
+
+#Executa o gráfico
+graf_linha.show()
